9dd40484ea163c1776439620f2a2f85d703dd003
momunbaevam <93367394+momunbaevam@users.noreply.github.com>
2022-05-20 15:44:19 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+const WIDTH=500,
+    HEIGHT=500,
+
+    canvas = document.getElement
+    context=canvas.getContext('2d')
+
+canvas.width=WIDTH;
+canvas.height=HEIGHT;
+canvas.style.background="123456";
+
+
+/*function addBlock(){
+    const block = document.createElement('div')
+    block.classList.add('block')
+    block.style.left ='100px'
+    block.style.left ='50px'
+    grid.appendChild(block)
+}*/
